388b01265c3c171808d4ad4f60070ede0a3eb919
Leo <LeoZhang.gmail.com>
2022-12-07 20:19:42 -0500

    Initial commit

---
+++ data/event.js
@@ -0,0 +1,45 @@
+//users can opreate
+const getSportsEvents = async(
+
+) => {}
+
+const getArtEvents = async(
+
+) => {}
+
+const getConcertEvents = async(
+
+) => {}
+
+const searchByDate = async(
+    date
+) => {}
+
+const searchByEventName = async(
+    name
+) => {}
+
+
+//manager can opreate
+const addEvent = async(
+    type,
+    name,
+    capacity,
+    date
+) => {}
+
+const deleteEvent = async(
+    id
+) => {}
+
+const updateEvent = async(
+    type,
+    name,
+    capacity,
+    date
+) => {}
+
+const addUerOfEvent = async(
+    eventName,
+    userName
+)
---
+++ data/users.js
@@ -0,0 +1,39 @@
+
+
+
+//createUser
+const createUser = async (
+    username, 
+    password,
+    firstName,
+    lastName,
+    age,
+    gender
+  ) => {}
+
+  //checkUser
+  const checkUser = async (
+    username, 
+    password
+  ) => {}
+
+  //user register event
+  const userRegisterEvent = async(
+    uerName,
+    eventName
+  ) => {}
+
+  //post review for events
+  const createReview = async (
+    userName,
+    eventName,
+    review,
+    rating
+  ) => {}
+
+
+
+  const getUerByName = async (
+    userName,
+  ) => {}
+  
